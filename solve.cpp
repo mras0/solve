5d62bf19161780f525903921754dbad3f4d1574b
Michael Rasmussen <mras@diku.dk>
2014-11-22 18:59:38 +0100

    More tests

--- solve.cpp
+++ solve.cpp
@@ -66,14 +66,14 @@ static size_t try_parse(token_type t, const char* text, size_t length) {
 class token {
 public:
     static const size_t nts = size_t(-1);
-    static token eof_token() { return token{token_type::eof, "", 0}; }
-    static token identifier_token(const char* str, size_t len = nts) {
+    static token eof() { return token{token_type::eof, "", 0}; }
+    static token identifier(const char* str, size_t len = nts) {
         return token{token_type::identifier, str, len == nts ? strlen(str) : len};
     }
-    static token literal_token(const char* str, size_t len = nts) {
+    static token literal(const char* str, size_t len = nts) {
         return token{token_type::literal, str, len == nts ? strlen(str) : len};
     }
-    static token op_token(const char* str, size_t len = nts) {
+    static token op(const char* str, size_t len = nts) {
         return token{token_type::op, str, len == nts ? strlen(str) : len};
     }
 
@@ -108,7 +108,7 @@ std::ostream& operator<<(std::ostream& os, const token& t) {
 
 class tokenizer {
 public:
-    tokenizer(const char* text, size_t length) : text_(text), length_(length), pos_(0), current_(token::eof_token()) {
+    tokenizer(const char* text, size_t length) : text_(text), length_(length), pos_(0), current_(token::eof()) {
         next_token();
     }
 
@@ -136,7 +136,7 @@ private:
             // handle end of text
             if (pos_ >= length_) {
                 assert(pos_ == length_);
-                current_ = token::eof_token();
+                current_ = token::eof();
                 return;
             }
 
@@ -169,7 +169,7 @@ void test_tokenizer(const std::string& text, std::initializer_list<token> expect
     tokenizer t{text.c_str(), text.length()};
     for (const auto& expected_token : expected_tokens) {
         auto current_token = t.consume();
-        std::cout << current_token << " ";
+        std::cout << current_token << " " << std::flush;
         if (current_token != expected_token) {
             std::cerr << "Tokenizer error. Expeceted " << expected_token << " got " << current_token << std::endl;
             abort();
@@ -181,15 +181,14 @@ void test_tokenizer(const std::string& text, std::initializer_list<token> expect
 
 int main()
 {
-    test_tokenizer("",{token::eof_token()});
-    test_tokenizer("\t\n\r   ",{token::eof_token()});
-    test_tokenizer(" id  ",{token::identifier_token("id"), token::eof_token()});
-    test_tokenizer("3.14",{token::literal_token("3.14"), token::eof_token()});
-    test_tokenizer("=\n",{token::op_token("="), token::eof_token()});
-    test_tokenizer("\thello 42",{token::identifier_token("hello"), token::literal_token("42"), token::eof_token()});
-    test_tokenizer("\tx + 1e3 = 20",{token::identifier_token("x"), token::op_token("+"), token::literal_token("1e3"), token::op_token("="), token::literal_token("20"), token::eof_token()});
+    test_tokenizer("",{token::eof()});
+    test_tokenizer("\t\n\r   ",{token::eof()});
+    test_tokenizer(" id  ",{token::identifier("id"), token::eof()});
+    test_tokenizer("3.14",{token::literal("3.14"), token::eof()});
+    test_tokenizer("=\n",{token::op("="), token::eof()});
+    test_tokenizer("\thello 42",{token::identifier("hello"), token::literal("42"), token::eof()});
+    test_tokenizer("\tx + 1e3 = 20",{token::identifier("x"), token::op("+"), token::literal("1e3"), token::op("="), token::literal("20"), token::eof()});
 
-#if 0
     const char* const program = R"(
         vals       = 2000
         valsize    = 8
@@ -197,6 +196,12 @@ int main()
         bspersec   = vals * valsize * freq
         bsperday   = bspersec * 60 * 60 * 24
     )";
-    test_tokenizer(program, {make_token(token_type
-#endif
+    test_tokenizer(program, {
+        token::identifier("vals"),     token::op("="), token::literal("2000"),
+        token::identifier("valsize"),  token::op("="), token::literal("8"),
+        token::identifier("freq"),     token::op("="), token::literal("1"),
+        token::identifier("bspersec"), token::op("="), token::identifier("vals"), token::op("*"), token::identifier("valsize"), token::op("*"), token::identifier("freq"),
+        token::identifier("bsperday"), token::op("="), token::identifier("bspersec"), token::op("*"), token::literal("60"), token::op("*"), token::literal("60"), token::op("*"), token::literal("24"),
+        token::eof(),
+    });
 }
