419d004b93c06c6c7a1c9a9001f9850cd86dce9b
Michael Rasmussen <mras@diku.dk>
2014-11-22 18:50:57 +0100

    De-virtualize token.

--- solve.cpp
+++ solve.cpp
@@ -5,21 +5,13 @@
 #include <memory>
 #include <initializer_list>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include <ctype.h>
 
-namespace ast {
-
-class node {
-public:
-    virtual ~node() {};
-};
-
-} // namespace ast
-
 enum class token_type {
-    literal,
     identifier,
+    literal,
     op,
 
     eof
@@ -28,8 +20,8 @@ enum class token_type {
 std::ostream& operator<<(std::ostream& os,  token_type t) {
     switch (t) {
 #define HANDLE_TOKEN_TYPE(t) case token_type::t: os << #t; break
-    HANDLE_TOKEN_TYPE(literal);
     HANDLE_TOKEN_TYPE(identifier);
+    HANDLE_TOKEN_TYPE(literal);
     HANDLE_TOKEN_TYPE(op);
     HANDLE_TOKEN_TYPE(eof);
 #undef HANDLE_TOKEN_TYPE
@@ -38,23 +30,68 @@ std::ostream& operator<<(std::ostream& os,  token_type t) {
 }
 
 
+static size_t try_parse(token_type t, const char* text, size_t length) {
+    assert(length != 0);
+    if (t == token_type::identifier) {
+        size_t l = 0;
+        while (l < length && isalpha(text[l])) {
+            ++l;
+        }
+        return l;
+    } else if (t == token_type::literal) {
+        std::istringstream iss{std::string{text, text+length}};
+        assert(iss.tellg() == 0);
+        double value;
+        if (iss >> value) {
+            const auto l = iss.eof() ? length : static_cast<size_t>(iss.tellg());
+            assert(l != 0);
+            assert(l <= length);
+            return l;
+        }
+    } else if (t == token_type::op) {
+        for (const auto& op : "*+-/=") {
+            if (*text == op) {
+                return 1;
+            }
+        }
+    } else {
+        std::ostringstream oss;
+        oss << "Unhandled token type '" << t << "' str='" << std::string{text, text+length} << "' in " << __func__;
+        throw std::logic_error(oss.str());
+    }
+    return 0;
+}
+
+
 class token {
 public:
-    virtual ~token() {}
+    static const size_t nts = size_t(-1);
+    static token eof_token() { return token{token_type::eof, "", 0}; }
+    static token identifier_token(const char* str, size_t len = nts) {
+        return token{token_type::identifier, str, len == nts ? strlen(str) : len};
+    }
+    static token literal_token(const char* str, size_t len = nts) {
+        return token{token_type::literal, str, len == nts ? strlen(str) : len};
+    }
+    static token op_token(const char* str, size_t len = nts) {
+        return token{token_type::op, str, len == nts ? strlen(str) : len};
+    }
 
     token_type  type() const { return type_; }
     std::string str() const { return std::string(text_, text_ + length_); }
     size_t      length() const { return length_; }
-protected:
+private:
+    friend class tokenizer;
+
     token(token_type type, const char* text, size_t length) : type_(type), text_(text), length_(length) {
+        assert((type == token_type::eof && length == 0) || try_parse(type, text, length) == length);
     }
 
-private:
     token_type  type_;
     const char* text_;
     size_t      length_;
+
 };
-typedef std::unique_ptr<token> token_ptr;
 
 bool operator==(const token& a, const token& b) {
     return a.type() == b.type() && a.str() == b.str();
@@ -65,115 +102,22 @@ bool operator!=(const token& a, const token& b) {
 }
 
 std::ostream& operator<<(std::ostream& os, const token& t) {
-    os << "<" << t.type() << " '" << t.str() << "'>";
+    os << "{" << t.type() << " '" << t.str() << "'}";
     return os;
-}
-
-class literal_token : public token {
-public:
-    static token_ptr try_parse(const char* text, size_t length) {
-        std::istringstream iss{std::string{text, text+length}};
-        assert(iss.tellg() == 0);
-        double value;
-        if (iss >> value) {
-            const auto l = iss.eof() ? length : static_cast<size_t>(iss.tellg());
-            assert(l != 0);
-            assert(l <= length);
-            return token_ptr{new literal_token{text, l, value}};
-        }
-        return nullptr;
-    }
-
-    double value() const { return value_; }
-private:
-    double value_;
-
-    literal_token(const char* text, size_t length, double value) : token(token_type::literal, text, length), value_(value) {
-    }
-};
-
-class identifier_token : public token {
-public:
-    static token_ptr try_parse(const char* text, size_t length) {
-        size_t l = 0;
-        while (l < length && isalpha(text[l])) {
-            ++l;
-        }
-        return l > 0 ? token_ptr{new identifier_token{text, l}} : nullptr;
-    }
-private:
-    identifier_token(const char* text, size_t length) : token(token_type::identifier, text, length) {
-    }
-};
-
-class op_token : public token {
-public:
-    static token_ptr try_parse(const char* text, size_t length) {
-        if (length == 0) {
-            assert(false);
-            return nullptr;
-        }
-        const char c = *text;
-        for (const auto& op : "*+-/=") {
-            if (c == op) {
-                return token_ptr{new op_token{text, 1}};
-            }
-        }
-        return nullptr;
-    }
-private:
-    op_token(const char* text, size_t length) : token(token_type::op, text, length) {
-        assert(length == 1);
-    }
-};
-
-class eof_token : public token {
-public:
-    explicit eof_token() : token(token_type::eof, "", 0) {
-    }
-};
-
-token_ptr make_token(token_type type, const std::string& str)
-{
-    token_ptr p;
-    if (type == token_type::literal) {
-        p = literal_token::try_parse(str.c_str(), str.length());
-    } else if (type == token_type::identifier) {
-        p = identifier_token::try_parse(str.c_str(), str.length());
-    } else if (type == token_type::op) {
-        p = op_token::try_parse(str.c_str(), str.length());
-    } else {
-        std::ostringstream oss;
-        oss << "Unhandled token type '" << type << "' str='" << str << "'";
-        throw std::logic_error(oss.str());
-    }
-
-    if (!p) {
-        std::ostringstream oss;
-        oss << "Unable to make token type '" << type << "' out of '" << str << "'";
-        throw std::logic_error(oss.str());
-    }
-    return p;
-}
-token_ptr make_token(token_type type)
-{
-    assert(type == token_type::eof);
-    return token_ptr{new eof_token{}};
 }
 
 class tokenizer {
 public:
-    tokenizer(const char* text, size_t length) : text_(text), length_(length), pos_(0) {
+    tokenizer(const char* text, size_t length) : text_(text), length_(length), pos_(0), current_(token::eof_token()) {
         next_token();
     }
 
-    const token& peek() const {
-        assert(current_);
-        return *current_;
+    const token& current() const {
+        return current_;
     }
 
-    token_ptr consume() {
-        auto cur = std::move(current_);
+    token consume() {
+        auto cur = current_;
         next_token();
         return cur;
     }
@@ -182,7 +126,7 @@ private:
     const char* text_;
     size_t      length_;
     size_t      pos_;
-    token_ptr   current_;
+    token       current_;
 
     // initialize current_ with appropriate token type from pos_ bytes into text_
     // advance pos_ by the tokens length
@@ -192,7 +136,7 @@ private:
             // handle end of text
             if (pos_ >= length_) {
                 assert(pos_ == length_);
-                current_ = make_token(token_type::eof);
+                current_ = token::eof_token();
                 return;
             }
 
@@ -206,16 +150,11 @@ private:
                 continue;
             }
 
-            static const decltype(&identifier_token::try_parse) parsers[] = {
-                &identifier_token::try_parse,
-                &literal_token::try_parse,
-                &op_token::try_parse,
-            };
-
-            for (const auto& p : parsers) {
-                if (current_ = p(text_pos, remaining)) {
-                    assert(current_->length() <= remaining);
-                    pos_ += current_->length();
+            for (const auto& t : { token_type::identifier, token_type::literal, token_type::op}) {
+                if (const auto l = try_parse(t, text_pos, remaining)) {
+                    assert(l <= remaining);
+                    pos_ += l;
+                    current_ = token{t, text_pos, l};
                     return;
                 }
             }
@@ -225,30 +164,30 @@ private:
     }
 };
 
-void test_tokenizer(const std::string& text, std::initializer_list<token_ptr> expected_tokens)
+void test_tokenizer(const std::string& text, std::initializer_list<token> expected_tokens)
 {
     tokenizer t{text.c_str(), text.length()};
     for (const auto& expected_token : expected_tokens) {
         auto current_token = t.consume();
-        std::cout << *current_token << " ";
-        if (*current_token != *expected_token) {
-            std::cerr << "Tokenizer error. Expeceted " << *expected_token << " got " << *current_token << std::endl;
+        std::cout << current_token << " ";
+        if (current_token != expected_token) {
+            std::cerr << "Tokenizer error. Expeceted " << expected_token << " got " << current_token << std::endl;
             abort();
         }
     }
-    assert(t.peek().type() == token_type::eof);
+    assert(t.current().type() == token_type::eof);
     std::cout << "OK!\n";
 }
 
 int main()
 {
-    test_tokenizer("",{make_token(token_type::eof)});
-    test_tokenizer("\t\n\r   ",{make_token(token_type::eof)});
-    test_tokenizer(" id  ",{make_token(token_type::identifier, "id"), make_token(token_type::eof)});
-    test_tokenizer("3.14",{make_token(token_type::literal, "3.14"), make_token(token_type::eof)});
-    test_tokenizer("=\n",{make_token(token_type::op, "="), make_token(token_type::eof)});
-    test_tokenizer("\thello 42",{make_token(token_type::identifier, "hello"), make_token(token_type::literal, "42"), make_token(token_type::eof)});
-    test_tokenizer("\tx + 1e3 = 20",{make_token(token_type::identifier, "x"), make_token(token_type::op, "+"), make_token(token_type::literal, "1e3"), make_token(token_type::op, "="), make_token(token_type::literal, "20"), make_token(token_type::eof)});
+    test_tokenizer("",{token::eof_token()});
+    test_tokenizer("\t\n\r   ",{token::eof_token()});
+    test_tokenizer(" id  ",{token::identifier_token("id"), token::eof_token()});
+    test_tokenizer("3.14",{token::literal_token("3.14"), token::eof_token()});
+    test_tokenizer("=\n",{token::op_token("="), token::eof_token()});
+    test_tokenizer("\thello 42",{token::identifier_token("hello"), token::literal_token("42"), token::eof_token()});
+    test_tokenizer("\tx + 1e3 = 20",{token::identifier_token("x"), token::op_token("+"), token::literal_token("1e3"), token::op_token("="), token::literal_token("20"), token::eof_token()});
 
 #if 0
     const char* const program = R"(
@@ -258,5 +197,6 @@ int main()
         bspersec   = vals * valsize * freq
         bsperday   = bspersec * 60 * 60 * 24
     )";
+    test_tokenizer(program, {make_token(token_type
 #endif
 }
