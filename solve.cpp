189f1d2a9931e5ab5698c7d4a1a9e8995a4e9470
Michael Rasmussen <mras@diku.dk>
2014-11-23 06:29:07 +0100

    Try to limit recursion..

--- solve.cpp
+++ solve.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <queue>
+#include <unordered_set>
+#include <functional>
 #include <assert.h>
 #include "ast.h"
 
@@ -12,10 +14,17 @@ void test()
     ast_test();
 }
 
+size_t hash_combine(size_t a, size_t b) {
+    // blah, blah, terrible, etc. etc.
+    return a ^ b;
+}
+
 class expr {
 public:
     virtual ~expr() {}
     virtual std::unique_ptr<expr> clone() const = 0;
+    virtual size_t hash() const = 0;
+    virtual bool equal(const expr& e) const = 0;
 
     operator std::unique_ptr<expr>() const {
         return clone();
@@ -35,11 +44,19 @@ private:
 
 typedef std::unique_ptr<expr> expr_ptr;
 
+template<typename T, typename E>
+const T* expr_cast(const E& e) {
+    return dynamic_cast<const T*>(&e);
+}
+
+
 class const_expr : public expr {
 public:
     explicit const_expr(double value) : value_(value) {}
     virtual std::unique_ptr<expr> clone() const override { return std::unique_ptr<expr>{new const_expr{value_}}; }
     double value() const { return value_; }
+    virtual size_t hash() const override { return std::hash<double>()(value_); }
+    virtual bool equal(const expr& e) const { auto ep = expr_cast<const_expr>(e); return ep && ep->value() == value(); }
 private:
     double value_;
     virtual void print(std::ostream& os) const override {
@@ -52,6 +69,8 @@ public:
     explicit var_expr(const std::string& name) : name_(name) {}
     std::string name() const { return name_; }
     virtual std::unique_ptr<expr> clone() const override { return std::unique_ptr<expr>{new var_expr{name_}}; }
+    virtual size_t hash() const override { return std::hash<std::string>()(name_); }
+    virtual bool equal(const expr& e) const { auto ep = expr_cast<var_expr>(e); return ep && ep->name() == name(); }
 private:
     std::string name_;
     virtual void print(std::ostream& os) const override {
@@ -67,6 +86,17 @@ public:
     char op() const { return op_; }
     virtual std::unique_ptr<expr> clone() const override {
         return std::unique_ptr<expr>{new bin_op_expr{lhs().clone(), rhs().clone(), op_}};
+    }
+    virtual size_t hash() const override {
+        return hash_combine(hash_combine(op(), lhs().hash()), rhs().hash());
+    }
+    virtual bool equal(const expr& e) const {
+        auto ep = expr_cast<bin_op_expr>(e);
+        if (!ep) return false;
+        if (ep->op() != op()) return false;
+        if (!ep->lhs().equal(lhs())) return false;
+        if (!ep->rhs().equal(rhs())) return false;
+        return true;
     }
 private:
     expr_ptr lhs_;
@@ -101,11 +131,6 @@ std::ostream& operator<<(std::ostream& os, const expr_ptr& e) {
     return os;
 }
 
-template<typename T, typename E>
-const T* expr_cast(const E& e) {
-    return dynamic_cast<const T*>(&e);
-}
-
 bool match_const(const expr& e, const double& v) {
     auto cp = expr_cast<const_expr>(e);
     return cp && cp->value() == v;
@@ -137,22 +162,39 @@ struct solver {
     // Solve the equation "e" for variable "v"
 private:
     typedef std::pair<expr_ptr, expr_ptr> item_type;
+    struct item_hash {
+        size_t operator()(const item_type& i) const {
+            return hash_combine(i.first->hash(), i.second->hash());
+        }
+    };
+    struct item_pred {
+        bool operator()(const item_type& a, const item_type& b) const {
+            return a.first->equal(*b.first) && a.second->equal(*b.second);
+        }
+    };
+
     std::string v_;
     std::queue<item_type> items_;
+    std::unordered_set<item_type, item_hash, item_pred> old_items_;
 
     void push_job(expr_ptr lhs, expr_ptr rhs) {
-        items_.push(make_pair(std::move(lhs), std::move(rhs)));
+        auto job = make_pair(std::move(lhs), std::move(rhs));
+        if (old_items_.find(job) != old_items_.end()) {
+            std::cout << "skipping " << *job.first << "=" << *job.second << std::endl;
+            return;
+        }
+        items_.push(std::move(job));
     }
 
     expr_ptr do_solve_1() {
         while (!items_.empty()) {
             auto& top = items_.front();
+            old_items_.emplace(top.first->clone(), top.second->clone());
             if (auto e = do_solve(*top.first, *top.second)) {
                 return e;
             }
             items_.pop();
         }
-        assert(false);
         return nullptr;
     }
 
@@ -244,4 +286,6 @@ int main()
     // - Prioritize jobs by "badness"[not good] ("clean" rhs good? less tree depth good?)
     // - Avoid doing the same work again in push_job (hash_map of finished jobs)
     // - Isolate each atom in turn, when find(lhs, *match_atom(rhs, the_atom)) returns false we're done
+    //
+    test_solve(var("x") * constant(4) + constant(10), var("y"), "x", 0);
 }
